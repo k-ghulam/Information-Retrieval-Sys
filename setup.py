bb41dfa761eb0b2a8a7483d393cbbe37250e25f9
k-ghulam <gkushki80@gmail.com>
2024-12-03 18:24:06 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative Ai Project',  # Add a comma here
+    version='0.0.0',  # Add a comma here
+    author='Ghulam',
+    author_email='gkushki80@gmail.com',
+    packages=find_packages(),  # Correct function name: find_packages
+    install_requires=[]  # Correct spelling for install_requires
+)
